4abdaf4b9b374c1f00cd1a3308d53097c4f74506
Garabet Aslo <98517170+Gaasll@users.noreply.github.com>
2022-09-12 11:16:53 +0200

    Update DatabaseNotFoundTest.java

--- test/java/se/kth/iv1350/amazingpos/startup/DatabaseNotFoundTest.java
+++ test/java/se/kth/iv1350/amazingpos/startup/DatabaseNotFoundTest.java
@@ -1,10 +1,51 @@
 package se.kth.iv1350.amazingpos.startup;
-package se.kth.iv1350.amazingpos.model*;
+//package se.kth.iv1350.amazingpos.model*;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class DatabaseNotFoundTest {
-    
+import se.kth.iv1350.amazingpos.integration.DatabaseException;
+import se.kth.iv1350.amazingpos.integration.ExternalInventorySystem;
+
+
+
+/**
+ * Tests a databse not connected
+ */
+
+class DatabaseExceptionTest {
+
+    private ExternalInventorySystem ext;
+    @BeforeEach
+    void setUp() {
+        ext = ExternalInventorySystem.getInstance();
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    /**
+     * Should be changed to getMessage override- we should make our own function for user messages.
+     */
+    @Test
+    void testGetAdminMessage() {
+        boolean databaseErrorCorrespondsToError = false;
+
+        try{
+            ext.getItem(6);
+        }
+        catch (DatabaseException dbExc){
+            String s = dbExc.getAdminMessage().substring(dbExc.getAdminMessage().indexOf('\n'));
+            System.out.println(s);
+            if(s.contains("Database not found")) databaseErrorCorrespondsToError = true;
+
+        }
+        catch (InvalidItemExceptionTest itemNoFound){
+            itemNoFound.getStackTrace();
+        }
+        assertTrue(databaseErrorCorrespondsToError, "Database error");
+
+    }
 }
